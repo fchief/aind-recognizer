f74f02d092a1a49b2dcb13cec5c7f993d18b49b1
Kok Leong Fong <kokleong.fong@seagate.com>
2018-05-26 15:58:18 +0800

    part2 completed

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -67,7 +67,6 @@ class SelectorBIC(ModelSelector):
     http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
     Bayesian information criteria: BIC = -2 * logL + p * logN
     """
-
     def select(self):
         """ select the best model for self.this_word based on
         BIC score for n between self.min_n_components and self.max_n_components
@@ -77,7 +76,22 @@ class SelectorBIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        best_bic = float("Inf")
+        best_model = None
+        for ns in range(self.min_n_components, self.max_n_components+1):
+            try:
+                temp_model = self.base_model(ns)
+                temp_logL = temp_model.score(self.X, self.lengths)
+                N = len(self.X)
+                p = ns*(ns-1)+(ns-1)+2*self.X.shape[1]*ns
+                bic = -2*temp_logL + p*np.log(N)
+                if bic < best_bic:
+                    best_bic = bic
+                    best_model = temp_model
+            except:
+                pass
+
+        return best_model
 
 
 class SelectorDIC(ModelSelector):
@@ -89,21 +103,71 @@ class SelectorDIC(ModelSelector):
     https://pdfs.semanticscholar.org/ed3d/7c4a5f607201f3848d4c02dd9ba17c791fc2.pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
+    # Calculate anti log likelihoods.
+    def calc_log_likelihood_other_words(self, model, other_words):
+        return [model[1].score(word[0], word[1]) for word in other_words]
+
+    def calc_best_score_dic(self, score_dics):
+        # Max of list of lists comparing each item by value at index 0
+        return max(score_dics, key = lambda x: x[0])
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        other_words = []
+        models = []
+        score_dics = []
+        for word in self.words:
+            if word != self.this_word:
+                other_words.append(self.hwords[word])
+        try:
+            for num_states in range(self.min_n_components, self.max_n_components + 1):
+                hmm_model = self.base_model(num_states)
+                log_likelihood_original_word = hmm_model.score(self.X, self.lengths)
+                models.append((log_likelihood_original_word, hmm_model))
+
+        # Note: Situation that may cause exception may be if have more parameters to fit
+        # than there are samples, so must catch exception when the model is invalid
+        except Exception as e:
+            # logging.exception('DIC Exception occurred: ', e)
+            pass
+        for index, model in enumerate(models):
+            log_likelihood_original_word, hmm_model = model
+            score_dic = log_likelihood_original_word - np.mean(self.calc_log_likelihood_other_words(model, other_words))
+            score_dics.append(tuple([score_dic, model[1]]))
+        return self.calc_best_score_dic(score_dics)[1] if score_dics else None
 
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
 
     '''
-
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection using CV
-        raise NotImplementedError
+        best_score = float("-inf")
+        best_model = None
+        for ns in range(self.min_n_components, self.max_n_components + 1):
+            if len(self.sequences) <= 1:
+                continue
+
+            split_method = KFold(n_splits=min(3, len(self.sequences)))
+
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                logL_scores = []
+                self.X, self.lengths = combine_sequences(cv_train_idx, self.sequences)
+                X_test, length_test = combine_sequences(cv_test_idx, self.sequences)
+                try:
+                    temp_model = self.base_model(ns)
+                    logL_scores.append(temp_model.score(X_test, length_test))
+                except:
+                    pass
+                if np.mean(logL_scores) > best_score:
+                    best_score = np.mean(logL_scores)
+                    best_model = temp_model
+
+        if not best_model:
+            best_model = self.base_model(self.n_constant)
+
+        return best_model
